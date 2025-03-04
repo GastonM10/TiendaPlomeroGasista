8ec92526957b5241597aed68172c38ee7161c3d3
Gaston Ojeda <gaston.ojeda@hotmail.com.ar>
2025-03-03 22:57:09 -0300

    Fix images path

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -70,7 +70,7 @@ const Carrito = () => {
                 <div className="d-flex row vh-100">
                     <div className="col-8 mt-4">
                         <div className="pt-5 pb-5 d-flex row position-sticky bg-white p-3 m-1 rounded justify-content-center" style={{ top: '20px' }}>
-                            <img style={{ width: '100px' }} src="./public/img/empty-cart.png" alt="Carrito vacío" />
+                            <img style={{ width: '100px' }} src="../img/empty-cart.png" alt="Carrito vacío" />
                             <p className="fs-5 text-center pt-2">Agregá productos para armar tu carrito</p>
                             <button className="btn btn-purchase m-1 col-4 fw-bold text-white" onClick={() => navigate('/')}>Continuar comprando</button>
                         </div>
